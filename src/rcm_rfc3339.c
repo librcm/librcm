91785cfb5ee271e4a204ede2c035c77560f68443
Frank Braun <frank@cryptogroup.net>
2020-08-18 02:18:52 +0000

    simplify control flow

--- src/rcm_rfc3339.c
+++ src/rcm_rfc3339.c
@@ -15,9 +15,10 @@ RCM_API int rcm_rfc3339_parse(rcm_rfc3339_t *time, const char *value, char *err)
 {
   unsigned long year, month, day, hour, minute, second;
   char *endptr;
-  int ret = 0;
+  struct tm tm = { 0 };
+  time_t t;
 
-  assert(time && value);
+  assert(value);
 
   /* try to parse value as YYYY-MM-DDTHH:MM:SSZ (as in ISO 8601 / RFC 3339) */
   if (strlen(value) != 20 || value[4] != '-' || value[7] != '-' ||
@@ -32,127 +33,99 @@ RCM_API int rcm_rfc3339_parse(rcm_rfc3339_t *time, const char *value, char *err)
                    "rcm_rfc3339: ill-formed value (must have format "
                    "\"YYYY-MM-DDTHH:MM:SSZ\"): %s",
                    value);
-    ret = -1;
+    return -1;
   }
   /* parse year */
-  if (!ret) {
-    year = strtoul(value, &endptr, 10);
-    if (endptr != value + 4) {
-      rcm_errbuf_set(err, "rcm_rfc3339: year could not be parsed: %s", value);
-      ret = -1;
-    }
+  year = strtoul(value, &endptr, 10);
+  if (endptr != value + 4) {
+    rcm_errbuf_set(err, "rcm_rfc3339: year could not be parsed: %s", value);
+    return -1;
   }
-  if (!ret) {
-    if (year < 1900 || year > 2525) {
-      rcm_errbuf_set(
-          err, "rcm_rfc3339: year must be in the range [1900,2525]: %s", value);
-      ret = -1;
-    }
+  if (year < 1900 || year > 2525) {
+    rcm_errbuf_set(
+        err, "rcm_rfc3339: year must be in the range [1900,2525]: %s", value);
+    return -1;
   }
 
   /* parse month */
-  if (!ret) {
-    month = strtoul(value + 5, &endptr, 10);
-    if (endptr != value + 7) {
-      rcm_errbuf_set(err, "rcm_rfc3339: month could not be parsed: %s", value);
-      ret = -1;
-    }
+  month = strtoul(value + 5, &endptr, 10);
+  if (endptr != value + 7) {
+    rcm_errbuf_set(err, "rcm_rfc3339: month could not be parsed: %s", value);
+    return -1;
   }
-  if (!ret) {
-    if (month < 1 || month > 12) {
-      rcm_errbuf_set(err, "rcm_rfc3339: month must be in the range [1,12]: %s",
-                     value);
-      ret = -1;
-    }
+  if (month < 1 || month > 12) {
+    rcm_errbuf_set(err, "rcm_rfc3339: month must be in the range [1,12]: %s",
+                   value);
+    return -1;
   }
   /* parse day */
-  if (!ret) {
-    day = strtoul(value + 8, &endptr, 10);
-    if (endptr != value + 10) {
-      rcm_errbuf_set(err, "rcm_rfc3339: day could not be parsed: %s", value);
-      ret = -1;
-    }
+  day = strtoul(value + 8, &endptr, 10);
+  if (endptr != value + 10) {
+    rcm_errbuf_set(err, "rcm_rfc3339: day could not be parsed: %s", value);
+    return -1;
   }
-  if (!ret) {
-    if (day < 1 || day > 31) {
-      rcm_errbuf_set(err, "rcm_rfc3339: day must be in the range [1,31]: %s",
-                     value);
-      ret = -1;
-    }
+  if (day < 1 || day > 31) {
+    rcm_errbuf_set(err, "rcm_rfc3339: day must be in the range [1,31]: %s",
+                   value);
+    return -1;
   }
   /* parse hour */
-  if (!ret) {
-    hour = strtoul(value + 11, &endptr, 10);
-    if (endptr != value + 13) {
-      rcm_errbuf_set(err, "rcm_rfc3339: hour could not be parsed: %s", value);
-      ret = -1;
-    }
+  hour = strtoul(value + 11, &endptr, 10);
+  if (endptr != value + 13) {
+    rcm_errbuf_set(err, "rcm_rfc3339: hour could not be parsed: %s", value);
+    return -1;
   }
-  if (!ret) {
-    if (hour > 23) {
-      rcm_errbuf_set(err, "rcm_rfc3339: hour must be in the range [0,23]: %s",
-                     value);
-      ret = -1;
-    }
+  if (hour > 23) {
+    rcm_errbuf_set(err, "rcm_rfc3339: hour must be in the range [0,23]: %s",
+                   value);
+    return -1;
   }
   /* parse minute */
-  if (!ret) {
-    minute = strtoul(value + 14, &endptr, 10);
-    if (endptr != value + 16) {
-      rcm_errbuf_set(err, "rcm_rfc3339: minute could not be parsed: %s", value);
-      ret = -1;
-    }
+  minute = strtoul(value + 14, &endptr, 10);
+  if (endptr != value + 16) {
+    rcm_errbuf_set(err, "rcm_rfc3339: minute could not be parsed: %s", value);
+    return -1;
   }
-  if (!ret) {
-    if (minute > 59) {
-      rcm_errbuf_set(err, "rcm_rfc3339: minute must be in the range [0,59]: %s",
-                     value);
-      ret = -1;
-    }
+  if (minute > 59) {
+    rcm_errbuf_set(err, "rcm_rfc3339: minute must be in the range [0,59]: %s",
+                   value);
+    return -1;
   }
   /* parse second */
-  if (!ret) {
-    second = strtoul(value + 17, &endptr, 10);
-    if (endptr != value + 19) {
-      rcm_errbuf_set(err, "rcm_rfc3339: second could not be parsed: %s", value);
-      ret = -1;
-    }
+  second = strtoul(value + 17, &endptr, 10);
+  if (endptr != value + 19) {
+    rcm_errbuf_set(err, "rcm_rfc3339: second could not be parsed: %s", value);
+    return -1;
   }
-  if (!ret) {
-    if (second > 59) {
-      rcm_errbuf_set(err, "rcm_rfc3339: second must be in the range [0,59]: %s",
-                     value);
-      ret = -1;
-    }
+  if (second > 59) {
+    rcm_errbuf_set(err, "rcm_rfc3339: second must be in the range [0,59]: %s",
+                   value);
+    return -1;
   }
   /* convert to time_t */
-  if (!ret) {
-    struct tm tm;
-    time_t t;
-    memset(&tm, 0, sizeof tm);
-    tm.tm_year = year - 1900;
-    tm.tm_mon = month - 1;
-    tm.tm_mday = day;
-    tm.tm_hour = hour;
-    tm.tm_min = minute;
-    tm.tm_sec = second;
-    t = mktime(&tm);
-    if (t == (time_t)-1) {
-      rcm_errbuf_set(err,
-                     "rcm_rfc3339: could not convert value to time (must have "
-                     "format \"YYYY-MM-DDTHH:MM:SSZ\"): %s",
-                     value);
-      ret = -1;
-    } else if (time) {
-      time->year = year;
-      time->mon = month;
-      time->day = day;
-      time->hour = hour;
-      time->min = minute;
-      time->sec = second;
-    }
+  tm.tm_year = year - 1900;
+  tm.tm_mon = month - 1;
+  tm.tm_mday = day;
+  tm.tm_hour = hour;
+  tm.tm_min = minute;
+  tm.tm_sec = second;
+  t = mktime(&tm);
+  if (t == (time_t)-1) {
+    rcm_errbuf_set(err,
+                   "rcm_rfc3339: could not convert value to time (must have "
+                   "format \"YYYY-MM-DDTHH:MM:SSZ\"): %s",
+                   value);
+    return -1;
   }
-  return ret;
+  if (time) {
+    time->year = year;
+    time->mon = month;
+    time->day = day;
+    time->hour = hour;
+    time->min = minute;
+    time->sec = second;
+  }
+  return 0;
 }
 
 RCM_API int rcm_rfc3339_now(rcm_rfc3339_t *t, char *err)
