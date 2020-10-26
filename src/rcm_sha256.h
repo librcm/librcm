f8349244f7015922b6d6d1e7545976ee7b96f43a
Frank Braun <frank@cryptogroup.net>
2020-10-26 12:01:12 +0000

    document sha256 module

--- src/rcm_sha256.h
+++ src/rcm_sha256.h
@@ -21,7 +21,7 @@ Implements the SHA-256 cryptographic hash function.
 #define RCM_SHA256_SIZE 32
 
 /* Calculate SHA-256 hash of in message of given inlen and store the result in
-   out. The output buffer out must be at least RCM_SHA256_SIZE large! */
+   out. The output buffer out must be at least `RCM_SHA256_SIZE` large! */
 RCM_API void rcm_sha256(unsigned char *out, const unsigned char *in,
                         size_t inlen);
 
